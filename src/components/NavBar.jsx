33db2b83c2989324ea75e12af2666672c1005ddd
Soledad Toledo <msoledadtoledo87@gmail.com>
2024-06-04 15:13:26 +0200

    cambio del logo del navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,13 +6,13 @@ const NavBar = () => {
         <nav className="bg-gray-800 p-4">
             <div className="container mx-auto flex justify-between items-center">
                 <div className="flex items-center">
-                    <img src="/public/logo1.png" alt="Logo" className="h-24 w-auto mr-2" /> {/* Agregando el logo */}
+                    <img src="/public/logo1.png" alt="Logo" className="h-24 w-auto mr-2 rounded-full" /> {/* Logo redondeado */}
                     <div className="text-white text-2xl font-bold">
                         <a href="/">E-Shop</a>
                     </div>
                 </div>
-                <div className="flex items-center space-x-8"> {/* Ajuste aquí */}
-                    <ul className="flex space-x-10"> {/* Más espacio entre los elementos */}
+                <div className="flex items-center space-x-8">
+                    <ul className="flex space-x-10">
                         <li>
                             <a 
                                 href="/" 
