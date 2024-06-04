446248d17aef8559c65914950f0e2c3b9d4150b0
Soledad Toledo <msoledadtoledo87@gmail.com>
2024-06-04 23:35:01 +0200

    modificaciones en components

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const CartWidget = () => {
+    const [productCount, setProductCount] = useState(1); 
+
     return (
-        <div className="text-white text-lg">
+        <div className="relative text-white text-lg">
             <a 
                 href="/cart" 
                 className="flex items-center hover:text-gray-500"
             >
-                <i className="fas fa-shopping-cart mr-2"></i>
-                
+                <i className="fas fa-shopping-cart fa-2x mr-2"></i> {/* Aumentando el tamaño del ícono */}
+                {productCount > 0 && (
+                    <span className="absolute top-0 right-0 bg-red-500 text-white rounded-full text-xs px-2 py-1">
+                        {productCount}
+                    </span>
+                )}
             </a>
         </div>
     );
