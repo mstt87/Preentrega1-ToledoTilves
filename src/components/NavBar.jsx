32dc7b1dbdc5ecca46dd0a422d7c47c86b1c2e3d
Soledad Toledo <msoledadtoledo87@gmail.com>
2024-06-04 15:05:32 +0200

    cambios en navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,41 +11,43 @@ const NavBar = () => {
                         <a href="/">E-Shop</a>
                     </div>
                 </div>
-                <ul className="flex space-x-4">
-                    <li>
-                        <a 
-                            href="/" 
-                            className="text-white text-lg hover:text-gray-300 hover:underline"
-                        >
-                            Home
-                        </a>
-                    </li>
-                    <li>
-                        <a 
-                            href="/products" 
-                            className="text-white text-lg hover:text-gray-300 hover:underline"
-                        >
-                            Products
-                        </a>
-                    </li>
-                    <li>
-                        <a 
-                            href="/about" 
-                            className="text-white text-lg hover:text-gray-300 hover:underline"
-                        >
-                            About
-                        </a>
-                    </li>
-                    <li>
-                        <a 
-                            href="/contact" 
-                            className="text-white text-lg hover:text-gray-300 hover:underline"
-                        >
-                            Contact
-                        </a>
-                    </li>
-                </ul>
-                <CartWidget />
+                <div className="flex items-center space-x-8"> {/* Ajuste aquí */}
+                    <ul className="flex space-x-10"> {/* Más espacio entre los elementos */}
+                        <li>
+                            <a 
+                                href="/" 
+                                className="text-white text-lg hover:text-gray-300 hover:underline"
+                            >
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a 
+                                href="/products" 
+                                className="text-white text-lg hover:text-gray-300 hover:underline"
+                            >
+                                Products
+                            </a>
+                        </li>
+                        <li>
+                            <a 
+                                href="/about" 
+                                className="text-white text-lg hover:text-gray-300 hover:underline"
+                            >
+                                About
+                            </a>
+                        </li>
+                        <li>
+                            <a 
+                                href="/contact" 
+                                className="text-white text-lg hover:text-gray-300 hover:underline"
+                            >
+                                Contact
+                            </a>
+                        </li>
+                    </ul>
+                    <CartWidget />
+                </div>
             </div>
         </nav>
     );
